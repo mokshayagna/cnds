bc403b2719c2a4df85dd804bd957156968583c63
bhagavan <bhagavan@auranetworks.in>
2023-06-14 09:40:07 +0530

    Adding new functions

---
+++ strings/astrncasecmp.c
@@ -0,0 +1,15 @@
+int astrcmp (char *ptr1, char *ptr2, int n)
+{
+	int i;
+
+	for(i=0; ptr1[i] != '\0' || ptr2[i] != '\0' || i < n; i++)
+	{
+		if(atolower(ptr1[i]) != atolower(ptr2[i]))
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
+
+
